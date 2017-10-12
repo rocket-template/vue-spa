1ed30ddeb2af4aca05de182ee8e05ae0b866e3fb
zeromike <540187842@qq.com>
2017-10-12 13:11:28 +0800

    fix(portablserver)

--- template/tools/portalServer.js
+++ template/tools/portalServer.js
@@ -4,24 +4,26 @@ const express = require('express');
 const middleware = require('rocket-portal/middleware/middleware');
 const pkgJson = require('../package.json');
 
-const app = express();
+module.exports = function () {
+    const app = express();
 
-app.set('views', path.join(__dirname, '../node_modules/rocket-portal/views'));
-app.use(express.static(path.join(__dirname, '../node_modules/rocket-portal/public')));
-app.use(require('express-blank-favicon'));
-middleware(express, app, {
-    root: path.join(__dirname, '../node_modules/rocket-portal'),
-    projectDescription: path.resolve(__dirname, '../README.md'),
-    pagePath: path.join(__dirname, '../node_modules/rocket-portal/views/index.html'),
-    buildEnvPath: path.resolve(__dirname, '../config/buildEnv.json'),
-    packageFile: path.resolve(__dirname, '../package.json')
-});
+    app.set('views', path.join(__dirname, '../node_modules/rocket-portal/dist'));
+    app.use(express.static(path.join(__dirname, '../node_modules/rocket-portal/dist/')));
 
-const server = http.createServer(app);
+    app.use(require('express-blank-favicon'));
+    middleware(express, app, {
+        root: path.join(__dirname, '../node_modules/rocket-portal'),
+        projectDescription: path.resolve(__dirname, '../README.md'),
+        buildEnvPath: path.resolve(__dirname, '../config/buildEnv.json'),
+        packageFile: path.resolve(__dirname, '../package.json')
+    });
 
-server.listen(pkgJson.rocket.portalPort, (err) => {
-    if (err) {
-        console.error(err);
-    }
-    console.log(`The portal server is listening at http://localhost:${pkgJson.rocket.portalPort}/`);
-});
+    const server = http.createServer(app);
+
+    server.listen(pkgJson.rocket.portalPort, (err) => {
+        if (err) {
+            console.error(err);
+        }
+        console.log(`The portal server is listening at http://localhost:${pkgJson.rocket.portalPort}/`);
+    });
+}
